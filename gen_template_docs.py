b78e4ea82e0ac965244812a96e095aeff9b5dc57
rcernich <rcernich@redhat.com>
2018-01-05 11:03:54 -0700

    update image tags for 1.4.8 release (sprint 13)
    Signed-off-by: rcernich <rcernich@redhat.com>

--- gen_template_docs.py
+++ gen_template_docs.py
@@ -24,15 +24,15 @@ APPLICATION_DIRECTORIES = ("amq","eap","webserver","decisionserver","processserv
 template_dirs = [ 'amq', 'eap', 'secrets', 'webserver', 'decisionserver', 'processserver', 'datagrid', 'datavirt', 'sso']
 amq_ssl_desc = None
 
-LINKS =  {"jboss-eap64-openshift:1.6": "../../eap/eap-openshift{outfilesuffix}[`jboss-eap-6/eap64-openshift`]",
-          "jboss-eap70-openshift:1.6": "../../eap/eap-openshift{outfilesuffix}[`jboss-eap-7/eap70-openshift`]",
+LINKS =  {"jboss-eap64-openshift:1.7": "../../eap/eap-openshift{outfilesuffix}[`jboss-eap-6/eap64-openshift`]",
+          "jboss-eap70-openshift:1.7": "../../eap/eap-openshift{outfilesuffix}[`jboss-eap-7/eap70-openshift`]",
           "jboss-eap71-openshift:1.1": "../../eap/eap-openshift{outfilesuffix}[`jboss-eap-7-tech-preview/eap71-openshift`]",
           "jboss-webserver31-tomcat7-openshift:1.1": "../../webserver/tomcat7-openshift{outfilesuffix}[`jboss-webserver-3/webserver31-tomcat7-openshift`]",
           "jboss-webserver31-tomcat8-openshift:1.1": "../../webserver/tomcat8-openshift{outfilesuffix}[`jboss-webserver-3/webserver31-tomcat8-openshift`]",
-          "jboss-decisionserver64-openshift:1.1": "../../decisionserver/decisionserver-openshift{outfilesuffix}[`jboss-decisionserver-6/decisionserver64-openshift`]",
-          "jboss-processserver64-openshift:1.1": "../../processserver/processserver-openshift{outfilesuffix}[`jboss-processserver-6/processserver64-openshift`]",
-          "jboss-datavirt63-openshift:1.3": "../../datavirt/datavirt-openshift{outfilesuffix}[`jboss-datavirt-6/datavirt63-openshift`]",
-          "redhat-sso71-openshift:1.1": "../../sso/sso-openshift{outfilesuffix}[`redhat-sso-7/sso71-openshift`]",
+          "jboss-decisionserver64-openshift:1.2": "../../decisionserver/decisionserver-openshift{outfilesuffix}[`jboss-decisionserver-6/decisionserver64-openshift`]",
+          "jboss-processserver64-openshift:1.2": "../../processserver/processserver-openshift{outfilesuffix}[`jboss-processserver-6/processserver64-openshift`]",
+          "jboss-datavirt63-openshift:1.4": "../../datavirt/datavirt-openshift{outfilesuffix}[`jboss-datavirt-6/datavirt63-openshift`]",
+          "redhat-sso71-openshift:1.3": "../../sso/sso-openshift{outfilesuffix}[`redhat-sso-7/sso71-openshift`]",
 }
 
 PARAMETER_VALUES = {"APPLICATION_DOMAIN": "secure-app.test.router.default.local", \
